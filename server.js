5876d26ad1793d252c3ba599ffe87574470ac1ea
AhmedWaleedKalifa <ahmed2210waleed@gmail.com>
2024-12-24 11:32:04 +0200

    make the server render the pages content correctly

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 let http=require("http")
-
-let server=http.createServer((req,res)=>{
-    res.writeHead(200,{"Content-Type":"text/plain"})
-    res.end("Hello World!")
-    
-}).listen(8080,()=>{console.log("http://localhost:8080")})
+let fs=require("fs")
+let url=require("url")
+http.createServer((req,res)=>{
+    let q= url.parse(req.url,true)
+    let pathName=q.pathname
+    let fileName=""
+    if(pathName=="/"){
+        fileName="./index.html"
+    }else if(pathName=="/about"){
+        fileName="./about.html"
+    }else if(pathName=="/contact"){
+        fileName="./contact.html"
+    }else{
+        fileName="./404.html"
+    }
+    fs.readFile(fileName,(err,data)=>{
+        if(err){
+            res.writeHead(404,{"Content-Type":"text/html"})
+            return res.end("404 Not Found")
+        }
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.write(data)
+        return res.end()
+    })   
+}).listen(8080,()=>{console.log("server running in   http://localhost:8080")})
